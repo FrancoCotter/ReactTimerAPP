fc4933aa646770638c841c2fe165e64c6739e765
Francocotter <chockyeizo@hotmail.com>
2017-02-15 18:55:52 +0800

    Add automatic tests

---
+++ example.js
@@ -0,0 +1,11 @@
+// let jsonData ={
+// id:42,
+// status:'ok',
+// data:[876,5309]
+// };
+// let {id,status,data:number} = jsonData;
+// console.log(id,status,number);
+
+
+let [foo,[[bar],baz]] = [233,[[12],45]];
+console.log(foo)
